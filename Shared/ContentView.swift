04ccd8b4ceb8f765c83bdb9c2b5f507aed027b5e
minhquan1701 <64230078+minhquan1701@users.noreply.github.com>
2022-07-16 18:13:10 +0700

    book list view page

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,9 +9,9 @@
   RMIT University Vietnam
   Course: COSC2659 iOS Development
   Semester: 2022B
-  Assessment: Assignment 2
-  Author: Your name (e.g. Nguyen Van Minh)
-  ID: Your student id (e.g. 1234567)
+  Assessment: Assignment 1
+  Author: Hoang Minh Quan
+  ID: s3754450
   Created  date: dd/mm/yyyy (e.g. 31/07/2022)
   Last modified: dd/mm/yyyy (e.g. 05/08/2022)
   Acknowledgement: Acknowledge the resources that you use here.
@@ -19,14 +19,66 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var bookName: String = ""
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: true){
-            HStack {
-            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                BookCardView()
+        NavigationView {
+            ScrollView {
+                HStack {
+                    HStack {
+                        Image("Search")
+                        TextField("Harry Porter", text: $bookName)
+                        Image("Circle Fill Close")
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8) )
+                    .frame(width: .infinity, height: 36.0)
+                    .background(Color("bg-grey"))
+                .cornerRadius(10)
+                }
+                .padding(EdgeInsets(top: 28, leading: 20, bottom: 28, trailing: 20) )
+                
+                //List of Books Begins
+                ScrollView(.horizontal, showsIndicators: true){
+                    HStack (spacing: 16) {
+                        ForEach(0 ..< 5) { item in
+                            BookCardView()
+                        }
+                    }
+                    .padding(EdgeInsets(top: 28, leading: 20, bottom: 40, trailing: 20) )
+                }
+                //List of Books Ends
+                
+                //Break line
+                Rectangle()
+                    .foregroundColor(Color("bg-grey"))
+                    .frame(width: .infinity, height: 1)
+                    .padding(.horizontal, 20)
+                
+                
+                HStack (spacing: .infinity){
+                    Text("Found Something Worth Reading")
+                        .fontWeight(.bold)
+                        .font(.system(size: 24))
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Add to Wishlist")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                    }
+                    .padding(8)
+                    .background(Color("primary-500"))
+                    .foregroundColor(Color("primary-50"))
+                    .cornerRadius(4)
+                    
+                }
+
+               
+                
             }
-        }}
+            .navigationTitle("📚 My Book Wishlist")
+        }
         
     }
     
@@ -42,22 +94,39 @@ struct BookCardView: View {
     var body: some View {
         VStack{
             Image("the-subtle-art")
-                .padding(.bottom)
+                .resizable()
+                .aspectRatio(0.9, contentMode: .fill)
+                .frame(width: .infinity)
             VStack(alignment: .leading, spacing: 4){
                 Text("The Subtle Art Of Not Giving A Fuck")
+                    .font(.system(size: 18))
                     .fontWeight(.bold)
-                
-                Text("See More")
-                    .font(.subheadline)
+                    
+                    
+                HStack(alignment: .center, spacing: 2) {
+                    Text("See More")
+                        .foregroundColor(Color("primary-500"))
+                        .font(.system(size: 16))
+                    .fontWeight(.bold)
+                    
+                    Image("arrow-right")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                        
+                    
+                    
+                }
+                    
             }
+            .padding(EdgeInsets(top: 8, leading: 12, bottom: 20, trailing: 0))
+            .lineLimit(1)
+            
             
         }
-        .padding(EdgeInsets(top: 16, leading: 20, bottom: 20, trailing: 20))
         
-        .frame(width: 300, height: 400)
-        .cornerRadius(100) //TODO: fix this
-        .background(Color.yellow.opacity(0.95)) //TODO: change this
-        
-//        .shadow(color: Color.black.opacity(0.4), radius: 0.6, y: 4) TODO: fix
+        .frame(width: 200, height: 300)
+        .background(Color("primary-50")) //TODO: change this
+        .cornerRadius(6) //TODO: fix this
+        .shadow(color: Color.black.opacity(0.12), radius: 16, x: 0,y: 8)
     }
 }
